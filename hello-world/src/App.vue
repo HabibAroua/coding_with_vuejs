cfd6a6fcf1e14694161ca28ca838f6cabfc77897
HabibAroua <habib.aroua@sesame.com.tn>
2023-10-17 17:00:06 +0100

    Binding Classes

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -5,6 +5,17 @@
   <div v-html="hack"></div>
   <h2 v-bind:id="headingId">Heading</h2>
   <button v-bind:disabled="isDisabled">Bind</button>
+  <h2 class="underline">Underlined text</h2>
+  <h2 class="underline" v-bind:class="status">Status</h2>
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new' " >SoldOut? movie</h2>
+  <h2 v-bind:class="['new', 'promoted']" >Newly promoted movie</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted',isSoldOut ? 'sold-out' : 'new']" >
+    Array conditional movie
+  </h2>
+  <h2 v-bind:class="{ promoted: isPromoted, new: !isSoldOut, 'sold-out': isSoldOut }" >
+    Object conditional movie
+  </h2>
 </template>
 
 <script>
@@ -20,7 +31,10 @@
         channel: "<b>Codeevolution</b>",
         hack: `<a href="#" onclick="alert('You have been hacked')">Win a prize!</a>`,
         headingId: 'heading',
-        isDisabled: false
+        isDisabled: false,
+        status: 'success',
+        isPromoted: true,
+        isSoldOut: true
       };
     }
 
@@ -28,12 +42,33 @@
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app 
+  {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .underline
+  {
+    text-decoration: underline;
+  }
+
+  .promoted
+  {
+    font-style: italic ;
+  }
+
+  .new 
+  {
+    color: olivedrab;
+  }
+
+  .sold-out 
+  {
+    color: red;
+  }
 </style>
